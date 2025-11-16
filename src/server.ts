a98a245b002a23fd4e094b58dd0225ddbcb1356a
swapnil2000 <swapnilsharma0220@gmail.com>
2025-11-16 11:29:04 +0530

    db changes

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,22 @@
+
 import dotenv from 'dotenv';
 dotenv.config();
 
 import app from './app';
 import { liveUpdates } from './utils/liveUpdates';
+import { setMasterDbUrlFromSSM } from './utils/dbManager';
 
 const PORT = process.env.PORT || 4000;
 
-if (process.env.REDIS_URL) {
-  liveUpdates.configure(process.env.REDIS_URL);
-}
+(async () => {
+  // Fetch and set master DB URL from SSM before Prisma is used
+  await setMasterDbUrlFromSSM('/prod/master-db-url'); // <-- Change to your actual SSM parameter name
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  if (process.env.REDIS_URL) {
+    liveUpdates.configure(process.env.REDIS_URL);
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+})();
